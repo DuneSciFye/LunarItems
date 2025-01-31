7e23f40dba2f16f319e14b6bac9c8a2846360c61
DuneSciFye <dunescifye@gmail.com>
2025-01-30 20:08:03 -0500

    Reworked ResetItem Command

--- src/main/java/me/dunescifye/lunaritems/commands/ResetItemCommand.java
+++ src/main/java/me/dunescifye/lunaritems/commands/ResetItemCommand.java
@@ -1,46 +1,60 @@
 package me.dunescifye.lunaritems.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.ArgumentSuggestions;
 import dev.jorel.commandapi.arguments.MultiLiteralArgument;
+import dev.jorel.commandapi.arguments.PlayerArgument;
+import dev.jorel.commandapi.arguments.StringArgument;
 import me.dunescifye.lunaritems.LunarItems;
 import me.dunescifye.lunaritems.utils.Utils;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
+
+import java.util.List;
 
 public class ResetItemCommand {
 
     public static void register() {
+
+        PlayerArgument playerArg = new PlayerArgument("Player");
+        StringArgument slotArg = new StringArgument("Slot");
         new CommandAPICommand("resetitem")
             .withArguments(new MultiLiteralArgument("Function", "lore", "attributes"))
-            .executesPlayer((player, args) -> {
-                ItemStack item = player.getInventory().getItemInMainHand();
-                if (!item.hasItemMeta()) return;
-                String itemID = item.getItemMeta().getPersistentDataContainer().get(LunarItems.keyEIID, PersistentDataType.STRING);
-                if (itemID == null) return;
-                Utils.runConsoleCommands("ei refresh " + player.getName() + " " + itemID + " " + args.getByClass("Function", String.class).toUpperCase());
-                /*
-                Set<NamespacedKey> keys = container.getKeys();
-                for (NamespacedKey key : keys) {
-                    if (key.equals(LunarItems.keyEIID) || !key.value().startsWith("score-")) continue;
-                    if (container.has(key, PersistentDataType.STRING)) {
-                        String currentValue = container.get(key, PersistentDataType.STRING);
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " temp");
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " " + currentValue);
-                    } else if (container.has(key, PersistentDataType.DOUBLE)) {
-                        double currentValue = container.get(key, PersistentDataType.DOUBLE);
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " 432.324");
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " " + currentValue);
-                    } else if (container.has(key, PersistentDataType.INTEGER)) {
-                        int currentValue = container.get(key, PersistentDataType.INTEGER);
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " 3892798");
-                        Utils.runConsoleCommands("ei console-modification set variable " + player.getName() + " " + player.getInventory().getHeldItemSlot() + " " + key.value().substring(6) + " " + currentValue);
-                    }
-                }
-
-                 */
+            .withOptionalArguments(playerArg.combineWith(slotArg.replaceSuggestions(ArgumentSuggestions.strings(Utils.getItemSlots()))).withPermission("lunaritems.commands.resetitemadmin"))
+            .executesPlayer((p, args) -> {
+                ItemStack item = p.getInventory().getItemInMainHand();
+                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), "main");
+            })
+            .executes((sender, args) -> {
+                Player p = args.getByArgument(playerArg);
+                if (p == null) return;
+                String slot = args.getByArgument(slotArg);
+                ItemStack item = Utils.getInvItem(p, slot);
+                if (item == null) return;
+                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), slot);
             })
             .withPermission("lunaritems.command.resetlore")
             .register();
+
+    }
+
+
+
+    private static void updateLore(Player p, ItemStack item, String function, String slot) {
+        if (!item.hasItemMeta()) return;
+        String itemID = item.getItemMeta().getPersistentDataContainer().get(LunarItems.keyEIID, PersistentDataType.STRING);
+        if (itemID == null) return;
+        List<Component> oldLore = item.lore();
+        Utils.runConsoleCommands("ei refresh " + p.getName() + " " + itemID + " " + function);
+        ItemStack newItem = Utils.getInvItem(p, slot);
+        if (newItem == null) return;
+        List<Component> newLore = newItem.lore();
+        if (oldLore == null || newLore == null) return;
+        for (int i = newLore.size(); i < oldLore.size(); i++)
+            newLore.add(oldLore.get(i));
+        newItem.lore(newLore);
     }
 
 }
--- src/main/java/me/dunescifye/lunaritems/utils/Utils.java
+++ src/main/java/me/dunescifye/lunaritems/utils/Utils.java
@@ -22,12 +22,14 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
 
+import static me.dunescifye.lunaritems.utils.ConfigUtils.isInteger;
 import static org.bukkit.Bukkit.getServer;
 
 public class Utils {
@@ -211,6 +213,31 @@ public class Utils {
         }
 
         return blocks;
+    }
+
+    public static String[] getItemSlots() {
+        return new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "main", "mainhand", "off", "offhand", "cursor"};
+    }
+    public static ItemStack getInvItem(Player player, String input) {
+        PlayerInventory inv = player.getInventory();
+        if (isInteger(input)) {
+            int slot = Integer.parseInt(input);
+            return slot == -1 ? inv.getItemInMainHand() : inv.getItem(slot);
+        } else {
+            switch (input) {
+                case "main", "mainhand" -> {
+                    return inv.getItemInMainHand();
+                }
+                case "offhand", "off" -> {
+                    return inv.getItemInOffHand();
+                }
+                case "cursor" -> {
+                    return player.getItemOnCursor();
+                }
+            }
+        }
+
+        return null;
     }
 
 }
