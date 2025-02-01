e180013754a00c3ccf47b5d09e316df8c01a4d77
DuneSciFye <dunescifye@gmail.com>
2025-01-31 19:54:04 -0500

    Reworked ResetItem Command

--- src/main/java/me/dunescifye/lunaritems/commands/ResetItemCommand.java
+++ src/main/java/me/dunescifye/lunaritems/commands/ResetItemCommand.java
@@ -1,13 +1,13 @@
 package me.dunescifye.lunaritems.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.arguments.ArgumentSuggestions;
-import dev.jorel.commandapi.arguments.MultiLiteralArgument;
-import dev.jorel.commandapi.arguments.PlayerArgument;
-import dev.jorel.commandapi.arguments.StringArgument;
+import dev.jorel.commandapi.arguments.*;
 import me.dunescifye.lunaritems.LunarItems;
 import me.dunescifye.lunaritems.utils.Utils;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
@@ -16,16 +16,21 @@ import java.util.List;
 
 public class ResetItemCommand {
 
+    private static final Component unbreakableLine = Component.text("").append(Component.text("Unbreakable", NamedTextColor.GRAY).decoration(TextDecoration.OBFUSCATED, false).decoration(TextDecoration.BOLD, false).decoration(TextDecoration.STRIKETHROUGH, false).decoration(TextDecoration.UNDERLINED, false).decoration(TextDecoration.ITALIC, false));
+
     public static void register() {
 
         PlayerArgument playerArg = new PlayerArgument("Player");
         StringArgument slotArg = new StringArgument("Slot");
+        IntegerArgument loreLinesArg = new IntegerArgument("Lore Lines");
+
         new CommandAPICommand("resetitem")
             .withArguments(new MultiLiteralArgument("Function", "lore", "attributes"))
             .withOptionalArguments(playerArg.combineWith(slotArg.replaceSuggestions(ArgumentSuggestions.strings(Utils.getItemSlots()))).withPermission("lunaritems.commands.resetitemadmin"))
+            .withOptionalArguments(loreLinesArg)
             .executesPlayer((p, args) -> {
                 ItemStack item = p.getInventory().getItemInMainHand();
-                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), "main");
+                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), "main", null);
             })
             .executes((sender, args) -> {
                 Player p = args.getByArgument(playerArg);
@@ -33,7 +38,7 @@ public class ResetItemCommand {
                 String slot = args.getByArgument(slotArg);
                 ItemStack item = Utils.getInvItem(p, slot);
                 if (item == null) return;
-                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), slot);
+                updateLore(p, item, args.getByClass("Function", String.class).toUpperCase(), slot, args.getByArgumentOrDefault(loreLinesArg, null));
             })
             .withPermission("lunaritems.command.resetlore")
             .register();
@@ -42,18 +47,24 @@ public class ResetItemCommand {
 
 
 
-    private static void updateLore(Player p, ItemStack item, String function, String slot) {
+    private static void updateLore(Player p, ItemStack item, String function, String slot, Integer endLine) {
         if (!item.hasItemMeta()) return;
         String itemID = item.getItemMeta().getPersistentDataContainer().get(LunarItems.keyEIID, PersistentDataType.STRING);
         if (itemID == null) return;
         List<Component> oldLore = item.lore();
+        if (oldLore == null) return;
+        int startLine = item.getType() == Material.FISHING_ROD ? oldLore.indexOf(unbreakableLine) : 0; // Keep Augments if Fishing Rod
         Utils.runConsoleCommands("ei refresh " + p.getName() + " " + itemID + " " + function);
         ItemStack newItem = Utils.getInvItem(p, slot);
         if (newItem == null) return;
         List<Component> newLore = newItem.lore();
-        if (oldLore == null || newLore == null) return;
-        for (int i = newLore.size(); i < oldLore.size(); i++)
-            newLore.add(oldLore.get(i));
+        if (newLore == null) return;
+        if (endLine == null) for (int i = newLore.size(); i < oldLore.size(); i++) // Add extra lore to New Lore
+            newLore.add(oldLore.get(i + startLine));
+        else for (int i = endLine; i < oldLore.size(); i++) // Add extra lore to New Lore with changed End line
+            newLore.add(oldLore.get(i + startLine));
+        for (int i = 0; i < startLine; i ++) // Add Augments to New Lore
+            newLore.add(i, oldLore.get(i));
         newItem.lore(newLore);
     }
 
